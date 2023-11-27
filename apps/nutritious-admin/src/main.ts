4c6e76b23812cf8294226020c2186c16383a5d29
Zen C. Melzer <z@dsea.net>
2023-11-27 02:55:08 +0100

    feat(admin): add session storage

--- apps/nutritious-admin/src/main.ts
+++ apps/nutritious-admin/src/main.ts
@@ -1,48 +1,83 @@
-import AdminJSFastify from '@adminjs/fastify';
+import AdminJSFastify, { AuthenticationOptions } from '@adminjs/fastify';
 import { Database, Resource } from '@adminjs/prisma';
+import { FastifySessionOptions } from '@fastify/session';
 import { PrismaClient } from '@prisma/client';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 
 import AdminJS from 'adminjs';
 import Fastify from 'fastify';
+import { AuthService } from './auth/auth.helper.js';
 import { componentLoader } from './resources/components.js';
 import { enumMap, resources } from './resources/setup.js';
 
 
+type Callback = ( err?:any ) => void;
+type CallbackSession = ( err:any, result?:Fastify.Session | null ) => void;
+
+
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
-BigInt.prototype.toJSON = function () { return this.toString(); };
+BigInt.prototype.toJSON = function(){ return this.toString(); };
 
 
-const PORT = 7880
+const PORT = 7880;
 
 const start = async () => {
 
 	const prisma = new PrismaClient();
-	AdminJS.registerAdapter({ Database, Resource })
+	AdminJS.registerAdapter( { Database, Resource } );
 
-	const app = Fastify({})
-	const admin = new AdminJS({
+	const app = Fastify( {} );
+	const admin = new AdminJS( {
 		rootPath: '/admin',
 		componentLoader,
 		resources:
 			Object.values( resources ).map(
 				( [ model, options, features ] ) =>
-					( { model, options, features, client:prisma, enumMap } ),
+					( { model, options, features, client: prisma, enumMap } ),
 			),
-	})
+	} );
 
-	await AdminJSFastify.buildRouter(
+	const cookieSecret = 'asdlknasdfjaposihferut34u5b09347trdghfvkjh';
+
+	const authService = new AuthService( prisma );
+	const auth:AuthenticationOptions = {
+		authenticate: ( email, password ) => authService.checkCredentials( { email }, password ),
+		cookieName: 'nutritious-admin',
+		cookiePassword: cookieSecret,
+	};
+
+	const sessionStore = new PrismaSessionStore( prisma, {
+		sessionModelName: 'adminSession',
+		dbRecordIdIsSessionId: false,
+	} );
+
+	const sessionOptions:FastifySessionOptions = {
+		store: sessionStore,
+		saveUninitialized: false,
+		secret: cookieSecret,
+		cookie: {
+			httpOnly: true,
+			secure: 'auto',
+		},
+	};
+
+
+	await AdminJSFastify.buildAuthenticatedRouter(
 		admin,
+		auth,
 		app,
-	)
+		sessionOptions,
+	);
 
-	app.listen({ port: PORT }, (err, addr) => {
-		if (err) {
-			console.error(err)
-		} else {
-			console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
+	app.listen( { port: PORT }, ( err, addr ) => {
+		if( err ){
+			console.error( err );
+		}else{
+			console.log( `AdminJS started on http://localhost:${ PORT }${ admin.options.rootPath }` );
 		}
-	})
-}
+	} );
+};
 
-start()
+start();

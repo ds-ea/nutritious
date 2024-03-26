b0056da29f164dd6cd1a64a471e8f2507204b9ea
Zen C. Melzer <z@dsea.net>
2024-03-26 22:07:55 +0100

    chore: remove obsolete adminjs evaluation code

--- apps/adminjs/src/resources/components.ts
+++
@@ -1,8 +0,0 @@
-import { ComponentLoader } from 'adminjs';
-
-export const componentLoader = new ComponentLoader();
-export const Components = {
-	QuestionCatalog: componentLoader.add('QuestionCatalog', '../components/question-catalog'),
-	TestComponent: componentLoader.add('TestComponent', '../components/test')
-};
-

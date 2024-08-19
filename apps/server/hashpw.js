6e7c0ea8ffea9e9a7ae6a5638a08b1eecb487bf2
Zen C. Melzer <z@dsea.net>
2024-08-19 02:20:40 +0200

    build: add store and publishing related things

--- apps/server/hashpw.js
+++ apps/server/hashpw.js
@@ -1,29 +1,29 @@
-#!/usr/bin/env np-S npx tsx
-
-import { hash } from 'argon2';
-import { env, stdin, stdout } from 'node:process';
-import { createInterface } from 'node:readline';
-
+const {hash} = require('argon2');
+const {env, stdin, stdout} = require('node:process');
+const readline = require('node:readline');
 
 
 const secretRaw = env['PW_SECRET'];
 if( !secretRaw ){
-	console.log( env );
-	console.warn( 'env var PW_SECRET missing' );
-	process.exit( 1 );
+	console.warn(env);
+	console.warn('env var PW_SECRET missing');
+	process.exit(1);
 }
 
-const rl = createInterface( { input: stdin, output: stdout } );
+const rl = readline.createInterface({input: stdin, output: stdout});
+rl.input.on('keypress', function( c, k ){
+	const len = rl.line.length;
+	readline.moveCursor(rl.output, -len, 0);
+	readline.clearLine(rl.output, 1);
+	for( let i = 0 ; i < len ; i++ ){
+		rl.output.write('*');
+	}
+});
+rl.question('enter the password: ', async str => {
+	const secret = Buffer.from(secretRaw, 'utf-8');
+	const hashedPass = await hash(str, {secret});
 
-rl.question( 'enter your password', async str => {
-
-
-	const secret = Buffer.from( secretRaw, 'utf-8' );
-	const hashedPass = await hash( str, { secret } );
-
-	console.log( 'PW_SECRET', env['PW_SECRET'] );
-	console.log( 'hash: ', hashedPass );
-
+	console.log('hash: ', hashedPass);
 
 	rl.close();
-} );
+});
